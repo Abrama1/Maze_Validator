e7732afcf0bbefd0e01f4c062381f0a84581b839
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-01-11 17:00:32 +0400

    Add ANSI color codes and define MAX_DIM for maze dimensions

--- Maze_Validator/Maze_Validator.cpp
+++ Maze_Validator/Maze_Validator.cpp
@@ -1,20 +1,16 @@
-// Maze_Validator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+const int MAX_DIM = 20; // Maximum maze dimensions
+
+// ANSI color codes
+const string RESET = "\033[0m";
+const string PLAYER_COLOR = "\033[32m"; // Green
+const string END_COLOR = "\033[31m";    // Red
+const string PATH_COLOR = "\033[34m";   // Blue
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
